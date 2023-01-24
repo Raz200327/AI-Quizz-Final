bc7c64b1ac0ed49e290c2dd9fe7f9ad298dc5534
Raz200327 <115984448+Raz200327@users.noreply.github.com>
2023-01-24 15:55:40 +1100

    Update notify_client.py

--- notify_client.py
+++ notify_client.py
@@ -12,7 +12,7 @@ def notify_user(receiving_email, link, name, quiz_name):
         html_content=f"<h1>Your Quiz about {quiz_name} is Ready!</h1><h3>Hey {name},</h3><p>"
                      f"Click to the link we sent you to go to your dashboard!</p><p>Let us know if you need any adjustments or if you're having trouble downloading it.</p>"
                      f"<p>Happy quizzing!</p>"
-                     f"<br><h2><a href='http://127.0.0.1:5050{link}' style='text-decoration: none;'>Start Quiz</a></h2>"
+                     f"<br><h2><a href='https://quizz.com.au{link}' style='text-decoration: none;'>Start Quiz</a></h2>"
     )
 
     sg = SendGridAPIClient(os.environ.get("EMAILAPIKEY"))
